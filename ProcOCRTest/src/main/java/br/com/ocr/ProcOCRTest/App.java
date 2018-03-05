1b418f0192ec70162a730a2cb6d8f766ac44d19b
gpanassol <gpanassol@gmail.com>
2018-03-05 13:43:35 -0300

    Estruturando projeto Maven

---
+++ ProcOCRTest/src/main/java/br/com/ocr/ProcOCRTest/App.java
@@ -0,0 +1,13 @@
+package br.com.ocr.ProcOCRTest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
